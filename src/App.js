5956cb6345ea370676557bae0be62a79a7145431
Svitlana Trotsenko <trots.svit@gmail.com>
2023-06-21 22:00:50 +0100

    create Hello World

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <p>Hello, world! I'm Frontend Developer!</p>
+        </div>
+    );
+}
+
+export default App;
